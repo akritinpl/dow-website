2a5bb1ba9fcfe6f7cb327c88c0de69ecf7ee3a9f
akritinpl <139466603+akritinpl@users.noreply.github.com>
2025-05-19 17:19:26 -0400

    Homepage Setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <header className='navbar'>
+        <nav>
+          <h1>Doctors on Wheels</h1>
+          <a href="#about">About Us</a>
+          <a href="#collab">Collaborators</a>
+          <a href="#research">Research</a>
+          <a href="#opps">Opportunities</a>
+          <a href="#telemed">Telemedicine</a>
+          <a href="https://downepal.blogspot.com/?fbclid=IwY2xjawKYfKpleHRuA2FlbQIxMABicmlkETFRUkpaM1ZNZ3NVV1phZWhBAR5kI8OxCkibykBP_owMG5b7zHcl_PYSJOdEG7Wl7WQd0L3ZwwDFvm9cY1SoNA_aem_GFbb6Y8DqPxQs3xklEoK4A" 
+          target="_blank" rel="noopener noreferrer">Our Blog</a>
+        </nav>
+      </header>
+
+      <footer>
+        <p>&copy; {new Date().getFullYear()} Doctors on Wheels</p>
+      </footer>
+    </div>
   )
 }
 
