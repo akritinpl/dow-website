5fb1dfa60a7412dfaf67fa00e93f544d857a8291
akritinpl <139466603+akritinpl@users.noreply.github.com>
2025-05-29 21:25:31 -0400

    Added a Contact Us Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,10 @@
 function About() {
-  return <h2>Welcome to the About Page</h2>;
+  return (
+    <>
+      <h2 className = "text-center">About Us</h2>
+      <p className = "pt-4">DOW was launched to contribute towards the upliftment of the people living in rural Nepal in the field of primary health care and increase awareness about preventive medicine.</p>
+    </>
+  );
 }
 
 export default About;
